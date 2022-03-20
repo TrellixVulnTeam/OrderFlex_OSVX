7a31f46d879872276597637fcaab41e2a90fd6e6
Luis Filipe Andrade <lfilipepython@gmail.com>
2022-03-20 19:32:39 -0300

    Multipage created

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+from multiapp import MultiApp
+from apps import home, data, model # import your app modules here
 
-names = ['John Smith','Rebecca Briggs','lipe']
-usernames = ['jsmith','rbriggs']
+app = MultiApp()
+
+names = ['John Smith','Rebecca Briggs','Luis Filipe']
+usernames = ['jsmith','rbriggs','luis']
 passwords = ['123','456','1995']
 
 hashed_passwords = stauth.Hasher(passwords).generate()
-
 
 authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
     'some_cookie_name','some_signature_key',cookie_expiry_days=30)
@@ -15,8 +18,10 @@ authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
 name, authentication_status, username = authenticator.login('Login','main')
 
 if authentication_status:
-    st.write('Welcome *%s*' % (name))
-    st.title('Some content')
+    app.add_app("Home", home.app)
+    app.add_app("Data", data.app)
+    app.add_app("Model", model.app)
+    app.run()
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
