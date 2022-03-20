7afdd6d6d519537865589455e83d7cd0f0b45e8d
Luis Filipe Andrade <lfilipepython@gmail.com>
2022-03-20 19:04:33 -0300

    login function auth with streamlit_authenticator

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+
+names = ['John Smith','Rebecca Briggs','lipe']
+usernames = ['jsmith','rbriggs']
+passwords = ['123','456','1995']
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+
+authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+
+
+name, authentication_status, username = authenticator.login('Login','main')
+
+if authentication_status:
+    st.write('Welcome *%s*' % (name))
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
